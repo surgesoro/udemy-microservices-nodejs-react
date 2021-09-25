5a8b3750ceb3637f1221d6273c1ce521a80524be
Surge <surgesoro@gmail.com>
2021-09-25 07:51:19 -0400

    Added Skaffold

--- blog/client/src/PostCreate.js
+++ blog/client/src/PostCreate.js
@@ -7,7 +7,7 @@ const PostCreate = () => {
   const onSubmit = async (event) => {
     event.preventDefault();
 
-    await axios.post("http://localhost:8080/posts", {
+    await axios.post("http://posts.com/posts/create", {
       title,
     });
 
