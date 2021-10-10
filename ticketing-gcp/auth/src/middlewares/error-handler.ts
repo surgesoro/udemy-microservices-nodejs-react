db70154f165f98fa0c6cb2f663e2baa00aeae5a3
Surge <surgesoro@gmail.com>
2021-10-10 10:10:56 -0400

    serialized error messages to be handled by ErrorHandler

--- ticketing-gcp/auth/src/middlewares/error-handler.ts
+++ ticketing-gcp/auth/src/middlewares/error-handler.ts
@@ -1,6 +1,5 @@
 import { Request, Response, NextFunction } from "express";
-import { RequestValidationError } from "../errors/request-validation-error";
-import { DatabaseConnectionError } from "../errors/database-connection-error";
+import { CustomError } from "../errors/custom-error";
 
 export const errorHandler = (
   err: Error,
@@ -8,15 +7,11 @@ export const errorHandler = (
   res: Response,
   next: NextFunction
 ) => {
-  if (err instanceof RequestValidationError) {
-    console.log("handling this error as a request validation error");
-    console.log(err);
+  if (err instanceof CustomError) {
+    return res.status(err.statusCode).send({ errors: err.serializeErrors() });
   }
 
-  if (err instanceof DatabaseConnectionError) {
-    console.log("handling this error as a db connection error");
-    console.log(err);
-  }
-
-  res.status(400).send({ message: err.message });
+  res
+    .status(500)
+    .send({ errors: [{ message: "Sorry, something went wrong :(" }] });
 };
